8b229c5fec1446e8e1bcaeffaa978308ac8336d1
wipfli <mail@example.com>
2020-04-11 09:02:32 +0200

    Add boot.py

---
+++ device/boot.py
@@ -0,0 +1,27 @@
+
+boot_config_content = '''
+dtparam=i2c_arm=on
+dtoverlay=i2c-gpio,bus=3
+disable_splash=1
+dtoverlay=pi3-disable-bt
+dtoverlay=sdtweak,overclock_50=100
+boot_delay=0
+force_turbo=1
+'''
+
+with open('/boot/config.txt', 'w') as f:
+    f.write(boot_config_content)
+
+boot_cmdline_content = ''
+with open('/boot/cmdline.txt', 'r') as f:
+    boot_cmdline_content = f.readline()
+
+boot_cmdline_content = boot_cmdline_content.strip()
+
+if 'quiet' not in boot_cmdline_content:
+    boot_cmdline_content += ' quiet\n'
+
+    with open('/boot/cmdline.txt', 'w') as f:
+        f.write(boot_cmdline_content)
+
+
