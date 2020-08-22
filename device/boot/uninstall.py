b22e36e8613073ebda3fc3c9c455feb512322e24
wipfli <email@example.com>
2020-08-22 14:27:13 +0000

    Remove boot files

--- device/boot/uninstall.py
+++
@@ -1,5 +0,0 @@
-import shutil
-
-shutil.move('/boot/config.txt.backup', '/boot/config.txt')
-shutil.move('/boot/cmdline.txt.backup', '/boot/cmdline.txt')
-shutil.move('/etc/modules.backup', '/etc/modules')
