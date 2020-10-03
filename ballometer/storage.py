d5d96a9e13c31c2b781bb20011368cf582d44fe0
wipfli <email@example.com>
2020-10-03 19:18:36 +0200

    Add volatile storage handlers which use redis

--- ballometer/storage.py
+++ ballometer/storage.py
@@ -1,13 +1,15 @@
 import influxdb
+import redis
 import time
 
 
 class Store:
     def __init__(self):
         self._db_name = 'ballometer'
-        self._client = influxdb.InfluxDBClient()
-        if self._client.switch_database(self._db_name) is None:
-            self._client.create_database(self._db_name)
+        self._influx = influxdb.InfluxDBClient()
+        if self._influx.switch_database(self._db_name) is None:
+            self._influx.create_database(self._db_name)
+        self._redis = redis.Redis()
 
     def save(self, key='key-name', value=1.0, unixtime=None):
         if unixtime is None:
@@ -19,12 +21,29 @@ class Store:
             # in year 1970. Skip writing to influxdb.
             return
 
-        self._client.write_points([
+        if not self.get_volatile_float('recording'):
+            # Recording has not been turned on (yet)
+            # by the user. Skip writing to influxdb.
+            return
+
+        self._influx.write_points([
             {
                 'measurement': key,
                 'fields': {
                     'value': float(value)
                 },
-                'time': unixtime
+                'time': unixtime,
+                'tags': {
+                    'flight_id': str(int(self.get_volatile_float('flight_id')))
+                }
             }
         ])
+
+    def get_volatile_float(self, key='key-name'):
+        value = self._redis.get(key)
+        if value is None:
+            return 0.0
+        return float(self._redis.get(key))
+
+    def set_volatile_float(self, key='key-name', value=1.0):
+        self._redis.set(key, value)
