aeb6d8585b8c1943545337b1d2fe4d483c0d31ff
wipfli <email@example.com>
2020-10-03 18:09:05 +0200

    Update run_ntp.py

--- run_ntp.py
+++ run_ntp.py
@@ -3,16 +3,20 @@ import struct
 import subprocess
 import time
 
+
 def set_system_time(unixtime=1601116701):
-    subprocess.run(['date', '-s', '@%i' % int(unixtime)], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
+    subprocess.run(['date', '-s', '@%i' % int(unixtime)],
+                   stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+
 def get_ntp_time():
     t = 0
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client.settimeout(3)
-        client.sendto(('\x1b' + 47 * '\0').encode('utf-8'), ('pool.ntp.org', 123))
-        msg, address = client.recvfrom(1024)
+        client.sendto(('\x1b' + 47 * '\0').encode('utf-8'),
+                      ('pool.ntp.org', 123))
+        msg, _ = client.recvfrom(1024)
         t = struct.unpack('!12I', msg)[10]
         time_1970 = 2208988800
         t -= time_1970
@@ -24,15 +28,15 @@ def get_ntp_time():
         t = 0
     except IndexError:
         t = 0
-        
+
     return t
-    
-if __name__ == '__main__':
-    time_was_set = False
-    while not time_was_set:
-        t = get_ntp_time()
-        if t > 0:
-            set_system_time(t)
-            time_was_set = True
-        else:
-            time.sleep(5)
+
+
+time_was_set = False
+while not time_was_set:
+    t = get_ntp_time()
+    if t > 0:
+        set_system_time(t)
+        time_was_set = True
+    else:
+        time.sleep(5)
