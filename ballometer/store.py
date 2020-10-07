259da312ae9243f7e11344d757c108989dd366fa
wipfli <email@example.com>
2020-10-07 20:35:03 +0200

    Hold in redis the last value saved in influx

--- ballometer/store.py
+++ ballometer/store.py
@@ -10,7 +10,7 @@ class Store:
         '''
         This constructor is blocking until both influxdb
         and redis respond to ping. This is useful during system
-        startup. When the constructor returns, the storage system 
+        startup. When the constructor returns, the storage system
         is ready to use.
         '''
 
@@ -18,9 +18,9 @@ class Store:
         self._influx = influxdb.InfluxDBClient()
         if self._influx.switch_database(self._db_name) is None:
             self._influx.create_database(self._db_name)
-            
+
         self._redis = redis.Redis()
-        
+
         while True:
             try:
                 self._influx.ping()
@@ -32,13 +32,13 @@ class Store:
             except redis.exceptions.ConnectionError:
                 print('Redis is not ready')
                 time.sleep(5)
-        
+
         if self._redis.get('flight_id') is None:
             # Volatile redis key has not been set yet.
             # Get it from influxdb.
             flight_id = self._get_max_flight_id()
             self._set_volatile_float('flight_id', float(flight_id))
-            
+
         if self._redis.get('qnh') is None:
             # Volatile redis key has not been set yet.
             # Get it from influxdb.
@@ -47,18 +47,21 @@ class Store:
 
     def clock_was_synchronized(self):
         return time.time() > 1601116701.0
-            
+
     def save(self, key='key-name', value=1.0, unixtime=None):
         '''
         Stores data permanently in influxdb if the system clock has been
-        synchronized (and is not at 1970 any more) and recording has 
+        synchronized (and is not at 1970 any more) and recording has
         been turned on.
         '''
         if unixtime is None:
             unixtime = time.time()
-            
-        self._redis.publish(f'save:{key}', 
-            json.dumps({'value': value, 'unixtime': unixtime}))
+
+        serialized = json.dumps({'value': value, 'unixtime': unixtime})
+        redis_key = f'save:{key}'
+        self._redis.publish(redis_key, serialized)
+        self._redis.set(redis_key, serialized)
+        self._redis.sadd('save', key)
 
         if not self.clock_was_synchronized():
             # The system time is not synchronized yet
@@ -84,6 +87,27 @@ class Store:
             }
         ])
 
+    def get_saved(self):
+        '''
+        Returns for all the keys the last value that was
+        saved in the format
+        {
+            'bmp_pressure': {
+                'value': 98443.0,
+                'unixtime': 123456.0
+            },
+            'sht_temperature': {
+                'value': 302.1,
+                'unixtime': 123456.0
+            },
+            ...
+        }
+        '''
+        return {
+            key.decode(): json.loads(self._redis.get(f'save:{key}'))
+            for key in self._redis.smembers('save')
+        }
+
     def _get_volatile_float(self, key='key-name'):
         value = self._redis.get(key)
         if value is None:
@@ -92,7 +116,7 @@ class Store:
 
     def _set_volatile_float(self, key='key-name', value=1.0):
         self._redis.set(key, str(float(value)))
-        
+
     def _get_max_flight_id(self) -> int:
         q = self._influx.query('SHOW TAG VALUES WITH KEY = "flight_id"')
         # list(q.get_points()) is [{'key': 'flight_id', 'value': '1'}, ...]
@@ -100,36 +124,37 @@ class Store:
         if len(values) == 0:
             return 0
         return max(values)
-    
+
     def _get_last_qnh(self) -> int:
-        q = self._influx.query('SELECT "value" FROM "qnh" ORDER BY DESC LIMIT 1')
-        # list(q.get_points()) is 
+        q = self._influx.query(
+            'SELECT "value" FROM "qnh" ORDER BY DESC LIMIT 1')
+        # list(q.get_points()) is
         # [{'time': '2020-10-03T18:10:00Z', 'value': 1018.0}]
         values = [int(point['value']) for point in q.get_points()]
         if len(values) == 0:
             return 1013
         return values[0]
-    
+
     @property
     def recording(self) -> bool:
         return bool(self._get_volatile_float('recording'))
-    
+
     @recording.setter
     def recording(self, value: bool):
         self._set_volatile_float('recording', float(value))
-        
+
     @property
     def flight_id(self) -> int:
         return int(self._get_volatile_float('flight_id'))
-    
+
     @flight_id.setter
     def flight_id(self, value: int):
         self._set_volatile_float('flight_id', float(value))
-        
+
     @property
     def qnh(self) -> int:
         return int(self._get_volatile_float('qnh'))
-    
+
     @qnh.setter
     def qnh(self, value: int):
         self.save('qnh', float(value))
--- tests/test_store.py
+++ tests/test_store.py
@@ -33,6 +33,26 @@ def test_save(mocker):
     store = ballometer.Store()
 
     mocker.patch('redis.Redis.publish')
+    mocker.patch('redis.Redis.sadd')
     mocker.patch('influxdb.InfluxDBClient.write_points')
 
     store.save(key='bmp_pressure', value=94034.0)
+
+def test_get_saved(mocker):
+    mocker.patch('redis.Redis.ping')
+    mocker.patch('influxdb.InfluxDBClient.ping')
+    mocker.patch('redis.Redis.get')
+    mocker.patch('redis.Redis.set')
+    mocker.patch('influxdb.InfluxDBClient.query')
+    store = ballometer.Store()
+    
+    def smembers(self, name):
+        if name == 'save':
+            return {b'bmp_pressure'}
+        else:
+            return {}
+        
+    mocker.patch('redis.Redis.smembers', smembers)
+    mocker.patch('redis.Redis.get', return_value='{"value": 94893.0, "unixtime": 123456}')
+    assert store.get_saved() == {'bmp_pressure': {'value': 94893.0, 'unixtime': 123456}}
+    
