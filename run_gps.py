7336cef9f91d08ca214aaa7cc46c9a0d480decc5
wipfli <email@example.com>
2020-10-27 22:42:00 +0000

    Update run_gps.py

--- run_gps.py
+++ run_gps.py
@@ -24,6 +24,28 @@ store = ballometer.Store()
 while True:
     gps.update()
     if gps.has_fix:
-        store.save(key='gps_latitude', value=gps.latitude)
-        store.save(key='gps_longitude', value=gps.longitude)
+        if gps.latitude is not None:
+            store.save(key='gps_latitude', value=gps.latitude)
+
+        if gps.longitude is not None:
+            store.save(key='gps_longitude', value=gps.longitude)
+        
+        if gps.altitude_m is not None:
+            store.save(key='gps_altitude', value=gps.altitude_m)
+
+        if gps.speed_knots is not None:
+            speed = gps.speed_knots * 0.514444  # m/s
+            store.save(key='gps_speed', value=speed)
+       
+        if gps.track_angle_deg is not None:
+            store.save(key='gps_heading', value=gps.track_angle_deg)
+
+        if gps.satellites is not None:
+            store.save(key='gps_satellites', value=gps.satellites)
+
+        if gps.horizontal_dilution is not None:
+            store.save(key='gps_horizontal_dilution', value=gps.horizontal_dilution)
+
         time.sleep(1)
+    else:
+        time.sleep(0.1)
