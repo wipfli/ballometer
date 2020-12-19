ab80af651aae427a308bc6b6a235f65b209438c0
wipfli <email@example.com>
2020-12-19 17:48:29 +0000

    Add backend for frontend endpoints /now and /before

--- run_store_server.py
+++ run_store_server.py
@@ -2,6 +2,8 @@ import uvicorn
 import fastapi
 import ballometer
 from fastapi.middleware.cors import CORSMiddleware
+import time
+
 
 app = fastapi.FastAPI()
 app.add_middleware(
@@ -68,7 +70,86 @@ def qnh() -> int:
     return store.qnh
 
 
-@app.get('/')
+@app.get('/now')
+def get_now():
+    '''
+        Returns the last measurement of altitude, speed,
+        heading, climb, longitude, and latitude:
+        {  
+            'altitude': 1464.343,
+            'speed': 8.6,
+            'heading': 234, 
+            'climb': 1.6,
+            'longitude': 8.43490,
+            'latitude': 43.4309
+            'time': 1608397940.45
+        }
+    '''
+    result = {}
+
+    point = store.get_saved()
+
+    ui_store_mapping = {
+        'altitude': 'vario_altitude',
+        'speed': 'gps_speed',
+        'heading': 'gps_heading', 
+        'climb': 'vario_speed',
+        'longitude': 'gps_longitude',
+        'latitude': 'gps_latitude'
+    }
+
+    for key in ui_store_mapping:
+        try:
+            result[key] = point[ui_store_mapping[key]]['value']
+        except KeyError:
+            result[key] = None
+
+    result['time'] = time.time()
+
+    return result
+
+@app.get('/before')
+def get_before():
+    '''
+    Returns the interpolated measurements of the current 
+    flight in the form:
+    {
+        'altitude': [918.8838187009885, 919.222839137572, ...], 
+        'speed': [12.3, 13.4, ...],
+        'climb': [0.5880960494320139, 0.5206506714967045, ...], 
+        'longitude': [8.43490, 8.43491, ...], 
+        'latitude': [43.64543, 43.645431, ...], 
+        'time': [1608369593.0, 1608369594.0, ...]
+    }
+    '''
+    result = {}
+
+    ui_store_mapping = {
+        'altitude': 'vario_altitude',
+        'speed': 'gps_speed',
+        'heading': 'gps_heading', 
+        'climb': 'vario_speed',
+        'longitude': 'gps_longitude',
+        'latitude': 'gps_latitude',
+        'time': 'time'
+    }
+
+    for key in ui_store_mapping:
+        result[key] = []
+
+    points = store.get_history()
+
+    for point in points:
+        for key in ui_store_mapping:
+            try:
+                result[key].append(point[ui_store_mapping[key]])
+            except KeyError:
+                result[key].append(None)
+
+    return result
+
+
+@app.get('/', response_class=fastapi.responses.HTMLResponse)
 def root():
     return '<p>Hello from store. <a href="docs">Docs</a>.</p>'
 
