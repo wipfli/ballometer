d40e411a0e2183774578fe4bbf0e881845d999a4
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2021-04-19 20:36:20 +0200

    Remove outdated files

--- board/ballometer/rootfs-overlay/root/ballometer/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='ballometer',
-      version='1.0.3',
-      description='The ballometer is a sensor box for hot air balloons',
-      url='https://github.com/wipfli/ballometer/',
-      author='wipfli',
-      python_requires='>=3.6',
-      packages=find_packages())
